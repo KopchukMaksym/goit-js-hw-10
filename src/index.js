c4e7755a5d9c794f5ab9598e205a9a44aadd1f25
Kopchuk Maksym <kopchuk.maksym@gmail.com>
2022-07-19 15:23:50 +0300

    fix bags

--- src/index.js
+++ src/index.js
@@ -10,34 +10,33 @@ const manyCountries = document.querySelector('.country-list');
 const oneCountry = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
 
-const onSearthCountry = event => {
+const onSearchCountry = () => {
   let searchCountry = inputEl.value.trim();
-  if (searchCountry.length === 0) {
-    oneCountry.remove();
-    manyCountries.remove();
-    return;
-  }
-  fetchCountries(searchCountry)
-    .then(data => {
-      if (data.length > 10) {
-        Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      } else if (data.length > 2 && data.length < 10) {
-        manyCountries.innerHTML = countryListsHbs(data);
-      } else if (data.length === 1) {
-        manyCountries.remove();
-        const country = data[0];
-        country.languages = Object.values(country.languages).join(',');
-        oneCountry.innerHTML = countryHbs(country);
-      }
-    })
 
-    .catch(err => {
-      if (err.message === '404') {
-        Notiflix.Notify.failure('Oops, there is no country with that name');
-      }
-    });
+  oneCountry.innerHTML = '';
+  manyCountries.innerHTML = '';
+
+  if (searchCountry.length > 0) {
+    fetchCountries(searchCountry)
+      .then(data => {
+        if (data.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (data.length > 2 && data.length < 10) {
+          manyCountries.innerHTML = countryListsHbs(data);
+        } else if (data.length === 1) {
+          const country = data[0];
+          country.languages = Object.values(country.languages).join(',');
+          oneCountry.innerHTML = countryHbs(country);
+        }
+      })
+      .catch(err => {
+        if (err.message === '404') {
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+        }
+      });
+  }
 };
 
-inputEl.addEventListener('input', debounce(onSearthCountry, DEBOUNCE_DELAY));
+inputEl.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
